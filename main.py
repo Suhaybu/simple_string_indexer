c86e60720aa277e86795d49963861cfbc79b1a98
Suhayb <suhayb@duck.com>
2023-06-24 21:43:55 +0300

    fixed exit

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ while choice == 'input':
     delimiter = ask.get_delimiter()
     user_input = ask.get_string()
     show.get_output(user_input, delimiter)
+    choice = ask.other_options()
 
-    while choice != 'exit' and choice == 'input':
-        choice = ask.other_options()
+    while choice != 'exit':
         print(f'choice is {choice}')
         if choice != 'input' and choice != 'exit':
             show.get_other_option(user_input, choice)
             print(f'end of choice {choice}')
+
 raise SystemExit("Exiting the program.")
