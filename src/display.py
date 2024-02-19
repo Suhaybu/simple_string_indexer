6f231a508ce499dbb18c012ea9bcdfcd3ac13f6c
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-02-20 00:18:56 +0300

    removed testing code

--- src/display.py
+++ src/display.py
@@ -1,25 +1,30 @@
 """
 Display introduces logic enabling logging to be bundled with print statements
 """
-import interface
 from colorama import Fore, Style  # noqa: F401
-from interface import (  # noqa: F401
-	enter,
-	error,
-	fancy_input,
-	greeting,
-	menu,
-	splitter,
-)
+
+history = ''
 
 
-def display(parameter: str):
-	if parameter is not None:
-		print(parameter)
+def display(new_data: str):
+	global history
+
+	if new_data is not None:
+		print(new_data)
+		history += new_data + '\n'
+
+		return history
 	else:
 		raise ValueError('The parameter is invalid. Make sure they exist.')
 
 
-# Testing
-display(interface.splitter('user input'))
-display(interface.splitter())
+# # Testing
+# display(interface.splitter('user input'))
+# # display(interface.splitter())
+# print('First History')
+# print(history)
+
+# display(interface.splitter('user input'))
+# display(interface.splitter('user input'))
+# print('Second History')
+# print(history)
